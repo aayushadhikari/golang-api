1f1ffca59eaa070b3c88766c5ce1a02f9b9a7496
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-07-11 17:33:10 -0400

    dummy

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,12 +9,35 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type blogPost struct {
+	title   string
+	content string
+}
+
+type blogPosts []blogPost
+
+var myPosts = blogPosts{
+	{
+		title:   "My Life",
+		content: "My Life is Good",
+	},
+	{
+		title:   "Your Life",
+		content: "Your Life is Good Too",
+	},
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeHandler)
+	router.HandleFunc("/posts", getPosts)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome Home")
+}
+
+func getPosts(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(myPosts)
 }
